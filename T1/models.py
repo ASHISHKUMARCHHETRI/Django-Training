1cac8b0b95eb992b8e08b06d56c5c84d358b2f94
Training <ashishkumarchhetri23@gmail.com>
2022-07-06 13:58:07 +0530

    added Teacher model in models

--- T1/models.py
+++ T1/models.py
@@ -5,5 +5,12 @@ class Employee(models.Model):
     name=models.CharField(max_length=50)
     experience=models.IntegerField()
     city=models.CharField(max_length=50)
+    def __str__(self):
+            return self.name
+class Teacher(models.Model):
+    name=models.CharField(max_length=50)
+    class_assigned=models.CharField(max_length=2)
+
+
 
 
